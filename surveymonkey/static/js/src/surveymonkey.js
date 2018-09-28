546a1ad0fff9bf865cbada911726c6eb396d156e
andrey <andrey.canon@edunext.co>
2018-09-28 17:50:32 -0500

    Add surveymonkey xblock

---
+++ surveymonkey/static/js/src/surveymonkey.js
@@ -0,0 +1,7 @@
+/* Javascript for SurveyMonkeyXBlock. */
+function SurveyMonkeyXBlock(runtime, element) {
+
+    $(function ($) {
+        /* Here's where you'd do things on page load. */
+    });
+}
