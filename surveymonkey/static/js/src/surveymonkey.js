8f75ce988a0aa78b61d4f79f96582c34c7bdbe09
andrey <andrey.canon@edunext.co>
2018-12-27 19:06:02 -0500

    remove unnecessary js functions

--- surveymonkey/static/js/src/surveymonkey.js
+++ surveymonkey/static/js/src/surveymonkey.js
@@ -4,21 +4,10 @@ function SurveyMonkeyXBlock(runtime, element) {
     var verifyCompletion = runtime.handlerUrl(element, "verify_completion");
 
     $(function ($) {
-        /* Here's where you'd do things on page load. */
-    });
-
-    $(".done", element).click(function(eventObject) {
-        $.ajax({
-            type: "GET",
-            url: verifyCompletion,
-            success: updateState
-        });
-    });
-
-    function updateState(data){
-        if(data.completed){
-            $(".done").hide();
-            $(".status-message").text(gettext("Completed"));
+        if(window.location.hash.startsWith("#completion")){
+            $(".surveymonkey_block_completion").show();
+        } else {
+            $(".surveymonkey_block").show();
         }
-    }
+    });
 }
